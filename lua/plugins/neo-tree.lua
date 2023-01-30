65a7af1f76599bc9c524b72daea51da8de0f361d
Alex Katz <alexanderikatz@gmail.com>
2023-01-30 16:40:40 -0500

    initial commit, added neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+print('i am here')
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+  },
+}
